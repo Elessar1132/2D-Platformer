9fd2825ebd825aa41b88f72e294fe72580cb0bbd
Christopher Stephens <ptcchrisnis@googlemail.com>
2018-01-16 02:20:51 +0000

    Player Health and Pickup Changes
    Renamed the PlayerHealth script to just Health so that it can be used on
    any entity (enemies etc.)
    Added the BasePickup class which should allow pickups to be written by
    just overriding the OnPickup virtual function

---
+++ Assets/Scripts/BasePickup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasePickup : MonoBehaviour
+{
+    delegate void PickupItem();
+    PickupItem pickupItem;
+	// Use this for initialization
+	void Awake ()
+    {
+        pickupItem += OnPickup;
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag.Equals("Player"))
+        {
+            // Player picked up this object
+            pickupItem();
+        }
+    }
+
+    protected virtual void OnPickup()
+    {
+        Debug.Log("OnPickup");
+    }
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField]
+    private int MaxHealth;
+    [SerializeField]
+    private int CurrentHealth;
+	
+    // Use this for initialization
+	void Start ()
+    {
+        CurrentHealth = MaxHealth;
+    }
+	
+	public void TakeDamage(int DamageToTake)
+    {
+        Mathf.Max(0, CurrentHealth - DamageToTake);
+
+        // TODO(Chris): Activate invulnerability?
+
+        if (CurrentHealth == 0)
+            Debug.Log("CurrentHealth == 0. Dead");
+    }
+
+    public void SetMaxHealth(int NewMaxHealth)
+    {
+        MaxHealth = NewMaxHealth;
+    }
+
+    public int GetMaxHealth()
+    {
+        return MaxHealth;
+    }
+
+    public void SetCurrentHealth(int NewCurrentHealth)
+    {
+        CurrentHealth = NewCurrentHealth;
+    }
+
+    public int GetCurrentHealth()
+    {
+        return CurrentHealth;
+    }
+}
